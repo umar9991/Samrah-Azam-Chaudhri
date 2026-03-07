8acfd8b88a1e0ddc9a1cde270f4b0a7b8774729c
umarahmad9991 <umarahmad9u8@gmail.com>
2026-03-08 01:23:58 +0400

    update frontend

--- src/hooks/use-articles.ts
+++ src/hooks/use-articles.ts
@@ -1,13 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { api } from "@shared/routes";
+import { STATIC_ARTICLES } from "@/data/static-data";
 
 export function useArticles() {
-  return useQuery({
-    queryKey: [api.articles.list.path],
-    queryFn: async () => {
-      const res = await fetch(api.articles.list.path, { credentials: "include" });
-      if (!res.ok) throw new Error("Failed to fetch articles");
-      return api.articles.list.responses[200].parse(await res.json());
-    },
-  });
+  return {
+    data: STATIC_ARTICLES,
+    isLoading: false,
+    isError: false,
+  };
 }
--- src/hooks/use-projects.ts
+++ src/hooks/use-projects.ts
@@ -1,13 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { api } from "@shared/routes";
+import { STATIC_PROJECTS } from "@/data/static-data";
 
 export function useProjects() {
-  return useQuery({
-    queryKey: [api.projects.list.path],
-    queryFn: async () => {
-      const res = await fetch(api.projects.list.path, { credentials: "include" });
-      if (!res.ok) throw new Error("Failed to fetch projects");
-      return api.projects.list.responses[200].parse(await res.json());
-    },
-  });
+  return {
+    data: STATIC_PROJECTS,
+    isLoading: false,
+    isError: false,
+  };
 }
